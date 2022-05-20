e1641b291f70a056850dc04335d73af59cd043c3
miroslavgenov <buchvata123@abv.bg>
2022-05-20 11:34:55 +0300

    Added student class for #1

---
+++ c++/MakeFile/SimpleMakeFileTest2/student.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <cstring>
+#include <iostream>
+
+class Student{
+  private:
+    char student_first_name[20];
+  public:
+    Student(){
+      strcpy(student_first_name,"Ivan");
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
