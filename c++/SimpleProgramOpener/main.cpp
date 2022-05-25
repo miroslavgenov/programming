3278f686945a09d2781288a52c611d84e7f77552
miroslavgenov <buchvata123@abv.bg>
2022-05-25 22:35:57 +0300

    add vec and doc

--- c++/SimpleProgramOpener/main.cpp
+++ c++/SimpleProgramOpener/main.cpp
@@ -5,51 +5,93 @@
 
 using namespace std;
 
+
+
+vector<string> initialize_menu(const string[],const size_t);
+
+template <typename T>
+void add_item_in_menu(vector<T> *source_menu,T new_item){
+    source_menu->push_back(new_item);
+}
+
+template<typename T>
+const int get_menu_size(vector<T> source_menu){
+    return source_menu.size();
+}
+
+template <typename T>
+T get_menu_item_by_index(vector<T> source_menu,const int MENU_INDEX){
+    return source_menu[MENU_INDEX];
+}
+
+/**
+ * @brief Show menu in a vector
+ * 
+ * @param src_menu Container whose values are printed
+ */
+void show_menu(vector<string> src_menu){
+    
+    for(int i=0;i<get_menu_size(src_menu);i++){
+        cout<<(i+1)<<"."<<get_menu_item_by_index(src_menu,i)<<endl;
+    }
+}
+
+
 int main(){
     // initial 
-    char menu_item1[]="Start";
-    char menu_item2[]="Settings";
-    char menu_item3[]="Exit";
-    const size_t MENU_SIZE=3;
-    const string MENU_PRIMERY[MENU_SIZE]={menu_item1,menu_item2,menu_item3};
-    char file[]="my.txt";
-    
-	ifstream input_file;
-    ofstream output_file;
+    const size_t menu_size=3;
+    string menu[menu_size]={"Star","Stop","Exit"};
+    vector<string> menu_primary=initialize_menu(menu,menu_size);
+    show_menu(menu_primary);
 
-    input_file.open(file);
+    
+
+    // const size_t MENU_LENGHT=3;
+	// string primary_menu[MENU_LENGHT]={"Start","Settings","Exit"};
+    // vector<string> p_menu;
+    // add_items_in_menu(&p_menu,primary_menu,MENU_LENGHT);
+    // show_menu(p_menu);
+	
+    //const size_t MENU_SIZE=3;
+    //const string MENU_PRIMERY[MENU_SIZE]={menu_item1,menu_item2,menu_item3};
+    //char file[]="my.txt";
+    
+	//ifstream input_file;
+    //ofstream output_file;
+
+    //input_file.open(file);
     
     // check if the file exists and creates new if doest
-    if(!input_file.is_open()){
-        cout<<"File doesnt exists"<<endl;
-        output_file.open(file);
-    	
-    	for(size_t i=0;i<MENU_SIZE;i++)	{
-    		if(i==2){
-    			output_file<<i+1<<"."<<MENU_PRIMERY[i];	
-			}else{
-				output_file<<i+1<<"."<<MENU_PRIMERY[i]<<endl;		
-			}
-			
-			
-		}
-	}
-	input_file.close();
-	
-	input_file.open(file);
-	
-	
-	ifstream iffile("my.txt");
-	
-	if(input_file.is_open()){
-		string line;
-		while(getline(input_file,line)){
-			cout<<line<<endl;
-		}
-		
-		
-	}
-	
+//    if(!input_file.is_open()){
+//        cout<<"File doesnt exists"<<endl;
+//        output_file.open(file);
+//    	
+//    	for(size_t i=0;i<MENU_SIZE;i++)	{
+//    		if(i==2){
+//    			output_file<<i+1<<"."<<MENU_PRIMERY[i];	
+//			}else{
+//				output_file<<i+1<<"."<<MENU_PRIMERY[i]<<endl;		
+//			}
+//			
+//			
+//		}
+//	}
+//	input_file.close();
+//	
+//	input_file.open(file);
+//	
+//	
+//	ifstream iffile("my.txt");
+//	
+//	if(input_file.is_open()){
+//		string line;
+//		while(getline(input_file,line)){
+//			cout<<line<<endl;
+//		}
+//		
+//		
+//	}
+//	
 	
 	
 	
@@ -64,6 +106,13 @@ int main(){
     
 }
 
+vector<string> initialize_menu(const string SOURCE_MENU[],const size_t SOURCE_MENU_LENGHT){
+    vector<string> menu;
+    for(size_t i=0;i<SOURCE_MENU_LENGHT;i++){
+        menu.push_back(SOURCE_MENU[i]);
+    }
+    return menu;
+}
 
 
 
