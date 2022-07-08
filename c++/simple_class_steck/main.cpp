fac29a023b171df1cddf8810cdee3e8ae1d8d90f
miroslavgenov <buchvata123@abv.bg>
2022-07-08 18:55:20 +0300

    add show in main
    function to print current , next and prev student if have

--- c++/simple_class_steck/main.cpp
+++ c++/simple_class_steck/main.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
 #include "student.cpp"
 
+void show(Student *list[],const int SIZE){
+	for(int i=0;i<SIZE;i++){
+		std::cout<<"Current student: "<<list[i]->get_name()<<std::endl;
+		
+		if(list[i]->get_prev_student()==nullptr)std::cout<<"prev null"<<std::endl;
+		else std::cout<<"Prev: "<<list[i]->get_prev_student()->get_name()<<std::endl;
+		
+		if(list[i]->get_next_student()==nullptr) std::cout<<"next null"<<std::endl;
+		else std::cout<<"Next: "<<list[i]->get_next_student()->get_name()<<std::endl;
+	}
+}
+
+
 int main(){
-    Student *list[5] = {
+	constexpr int SIZE=5;
+    Student *list[SIZE] = {
 		new Student("Ivan"),
 		new Student("Alex"),
 		new Student("Jotaro"),
@@ -25,15 +39,7 @@ int main(){
 		list[i]->set_prev_student(list[i-1]);
 	}
 	
-	for(auto item:list){
-		std::cout<<"Current student: "<<item->get_name()<<std::endl;
-		
-		if(item->get_prev_student()==nullptr)std::cout<<"prev null"<<std::endl;
-		else std::cout<<"Prev: "<<item->get_prev_student()->get_name()<<std::endl;
-		
-		if(item->get_next_student()==nullptr) std::cout<<"next null"<<std::endl;
-		else std::cout<<"Next: "<<item->get_next_student()->get_name()<<std::endl;
-	}
+	show(list,SIZE);
 	
 	
 	
