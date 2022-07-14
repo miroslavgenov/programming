b852f1d1d7dc33362e94f484dadade65c9f89f01
miroslavgenov <buchvata123@abv.bg>
2022-07-14 12:44:13 +0300

    more code to perfect number

--- c++/perfect_number/perfect_numbers.cpp
+++ c++/perfect_number/perfect_numbers.cpp
@@ -1,28 +1,83 @@
 #include <iostream>
 using namespace std;
 
-bool is_number_perfect(int *number){
-	if(*number<6 || number==nullptr){
-		return false;
-	}else if(*number>=6){
-		int sum=0;
-		for(int i=1;i<*number;i++)
-			sum+=*number%i==0?i:0;
-			
-		if(sum==*number)return true;
-		
-	}
-	return false;
+bool is_number_acceptable(int *);
+bool is_two_numbers_equal(int ,int);
+bool is_number_perfect(int *);
+void set_perfect_number_dividers_list(int [],int);
+int get_sum_of_perfect_number_dividers_list(int [],int);
+void print_perfect_nuber_dividers_list(int []);
+
+int perfect_number_dividers_list_size=0;
+
+
+void set_perfect_number_dividers_size(int val){
+	perfect_number_dividers_list_size=val;
+}
+
+int get_perfect_number_dividers_size(){
+	return perfect_number_dividers_list_size;
 }
 
 int main(){
 	int* number=new int(6);
+
 	cout<<is_number_perfect(number);
+	
 	
 }
 
 
 
+void print_perfect_nuber_dividers_list(int arr[],int size){
+	for(int i=0;i<size+1;i++){
+		cout<<arr[i]<<" ";
+	}
+}
+
+void set_perfect_number_dividers_list(int arr[],int perfect_number){
+	set_perfect_number_dividers_size(0);
+	int count=0;
+	for(int i=1;i<perfect_number;i++)
+			if(perfect_number%i==0){
+				arr[count]=i;
+				set_perfect_number_dividers_size(count++);
+			}
+}
+
+int get_sum_of_perfect_number_dividers_list(int arr[],int size){
+	int sum=0;
+	for(int i=0;i<size+1;i++)
+		sum=(sum,sum+arr[i]); // sum+=arr[i];
+		
+	return sum;
+	
+}
+
+bool is_number_acceptable(int *number_source){
+	if(*number_source<6 || number_source==nullptr)return false;
+	return true;
+}
+
+bool is_two_numbers_equal(int first_number,int second_number){
+	return first_number==second_number;
+}
+
+bool is_number_perfect(int *number_perfect){
+	
+	if(!is_number_acceptable(number_perfect)){
+		return false;
+	}else{
+		
+		int number_perfect_dividers_list[]{};
+		set_perfect_number_dividers_list(number_perfect_dividers_list,*number_perfect);
+		int sum=get_sum_of_perfect_number_dividers_list(number_perfect_dividers_list,get_perfect_number_dividers_size());
+		
+		return is_two_numbers_equal(sum,*number_perfect);
+	}
+	return false;
+	
+}
 
 
 
