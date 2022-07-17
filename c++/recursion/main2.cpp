0aaf161a9e2f64c9681720c5c9e17ee29bc69955
miroslavgenov <buchvata123@abv.bg>
2022-07-17 18:37:19 +0300

    solved full exercise lect 3

--- c++/recursion/main2.cpp
+++ c++/recursion/main2.cpp
@@ -3,38 +3,68 @@
 using namespace std;
 
 
-void row(int n,int i){
-	if(n==i) {return;}
-	
-	cout<<n<<' ';
-	row(++n,i);
-	cout<<n<<' ';
+void WOR(int n,int x){
+	if(n==x)return;
+	cout<<n<<" ";
+	WOR(++n,x);
+	cout<<n<<" ";
 	
 }
 
-void fig(int n,int i){
-	if(i==5)return;
-	
-	row(n,i);
-	cout<<1<<' ';
-	cout<<endl;
-	
-	fig(n,++i);
-	
-	cout<<endl;row(n,i);
-	
+void GIF(int n,int x,int z=5){
+	if(x==z)return;
+	WOR(n,x);
 	cout<<n<<" ";
-	if(n==(--i))cout<<'\n'<<1<<" ";
+	cout<<endl;
+	GIF(n,++x);
+	WOR(n,x);
+	cout<<n<<" ";
+	cout<<endl;
+	if(n==x-1)cout<<n<<" ";
+	
 		
 }
 
 
 
+void ROW(int n,int x){
+	if(n==x)return;
+	cout<<n<<' ';
+	ROW(--n,x);
+	cout<<n<<' ';
+	
+	
+}
+
+void FIG(int n,int x){
+	if(x==1)return;
+	ROW(n,x);
+	cout<<n<<" ";
+	cout<<endl;
+	FIG(n,--x);
+	
+	ROW(n,x);
+	cout<<n<<" ";
+	cout<<endl;
+	
+	if(n==x+1){
+		cout<<n<<" ";
+	}
+	
+
+	
+}
+
 
 
 int main(){
 
-fig(1,1);
+GIF(1,1);
+cout<<endl<<endl;
+FIG(5,5);
+
+
+
 
 
 
@@ -49,6 +79,15 @@ fig(1,1);
     
     
 }
+
+
+
+
+
+
+
+
+
 
 
 
