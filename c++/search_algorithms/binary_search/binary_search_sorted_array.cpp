1bbc33c8ddb3e944c84eada74b0e0dad5b17c016
miroslavgenov <buchvata123@abv.bg>
2022-07-23 12:16:39 +0300

    fixed binary search

--- c++/search_algorithms/binary_search/binary_search_sorted_array.cpp
+++ c++/search_algorithms/binary_search/binary_search_sorted_array.cpp
@@ -1,41 +1,67 @@
 #include <iostream>
 using namespace std;
 
-void b(int a[],int l,int m,int r,int v){
+void b(int a[],int l,int r,int v){
 
-	
 	if(l<=r){
+		int m=(l+r)/2;
+
 		if(a[m]==v) {
 			cout<<a[m]<<" "<<"found"<<endl;
+			return;
+		}
+		else if(a[m]<v){
+			b(a,m+1,r,v);
+		}
+		else if(a[m]>v){
+			b(a,l,m-1,v);
+		}
+	}
+	else{
 		return;
-	}
-	if(a[m]>v){
-		b(a,l,(l+m)/2,m-1,v);
-
-
-		
-
-	}
-	if(a[m]<v){
-		b(a,m+1,(m+r)/2,r,v);
-
-
-		
-	}
-	}else return;
+	} 
 	
 	
 }
 
+void bs1(int a[],int l,int r,int v){
+	cout<<"left= "<<l<<" right= "<<r<<endl;
+	if(l<=r){
+		cout<<"l<=r "<<l<<" <="<<r<<endl;
+		int mid=(l+r)/2;
+		cout<<"mid: "<<mid<<endl;
+		cout<<"a[mid]= "<<a[mid]<<endl;
+		cout<<"v= "<<v<<endl;
+		if(a[mid]==v){
+			cout<<a[mid]<<" == "<<v<<endl;
+			return;
+		}else if(a[mid]<v){
+			
+			cout<<a[mid]<<" < "<<v<<endl;
+			bs1(a,mid+1,r,v);
+		}else if(a[mid]>v){
+			cout<<a[mid]<<" > "<<v<<endl;
+			bs1(a,l,mid-1,v);
+			
+		}
+	}
+	else{
+
+		cout<<"else l>=r "<<l<<" >="<<r<<endl;
+		return;
+	}
+}
+
 
 int main(){
-    int array_numbers[]={0, 1 ,2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610};
-    int SIZE_ARRAY = sizeof(array_numbers)/sizeof(*array_numbers);
-	int searched_number=13;
+    int a[]={0, 1,1 ,2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610};
+    int s = sizeof(a)/sizeof(*a);
+	int sn=455;
 	
 		
-		b(array_numbers,0,(0+SIZE_ARRAY)/2,SIZE_ARRAY,searched_number);
-	
+		// b(a,0,(0+s)/2,s,sn);
+	// bs1(a,0,s-1,sn);
+	// b(a,0,s-1,sn);
 	
 	
 	
