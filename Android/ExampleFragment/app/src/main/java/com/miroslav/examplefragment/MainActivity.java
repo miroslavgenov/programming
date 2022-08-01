5058aaedb2bdb732d85abda7f70fccc585beebea
miroslavgenov <buchvata123@abv.bg>
2022-08-01 15:54:42 +0300

    edited some code

---
+++ Android/ExampleFragment/app/src/main/java/com/miroslav/examplefragment/MainActivity.java
@@ -0,0 +1,41 @@
+package com.miroslav.examplefragment;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        FragmentManager fragmentManager = getSupportFragmentManager();
+//        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+//
+//        Fragment exampleFragment = new ExampleFragment();
+//        fragmentTransaction.add(R.id.myContainer,exampleFragment);
+//        fragmentTransaction.commit();F
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Android/SimpleGymMemberMenage/app/src/main/java/com/miroslav/simpleGymMemberMenage/MainActivity.java
+++ Android/SimpleGymMemberMenage/app/src/main/java/com/miroslav/simpleGymMemberMenage/MainActivity.java
@@ -9,94 +9,73 @@ import android.os.Bundle;
 
 import com.miroslav.simpleGymMemberMenage.databinding.ActivityMainBinding;
 
-public class MainActivity extends AppCompatActivity implements  MyActivityBindingInterface,SharedPrefsInitializer{
 
-    ActivityMainBinding activityMainBinding ;
-    public static boolean isFinishSetActivity=false;
+public class MainActivity extends AppCompatActivity implements  MyActivityBindingInterface,SharedPrefsInitializer {
+
+    ActivityMainBinding activityMainBinding;
     MySharedPrefs mySharedPrefs;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setActivityBinding(DataBindingUtil.setContentView(this,R.layout.activity_main));
+
+        setActivityBinding(DataBindingUtil.setContentView(this, R.layout.activity_main));
         main();
-
-
-    }
-
+}
     private void main() {
 
         initializeSharedPrefs();
         boolean isSharedPrefsExists = mySharedPrefs.isSharedPrefsFileExists();
 
-        if(isSharedPrefsExists){
-
-            if(mySharedPrefs.getCardPriceFromSharedPrefs()!=0){
-
+        if (isSharedPrefsExists) {
+            if (mySharedPrefs.getCardPriceFromSharedPrefs() != 0) {
                 startMainMenuActivity();
-                finish();
-            }else{
+            } else {
                 setDefaultCardPrice(0);
                 startSetDefaultsActivity();
-                finish();
             }
-        }
-        else{
+        } else {
             setDefaultCardPrice(0);
             startSetDefaultsActivity();
-            finish();
-
         }
-
-
-
-
-
+        finish();
     }
 
-    private void startMainMenuActivity(){
+    private void startMainMenuActivity() {
         Intent intent = new Intent();
-        intent.setClass(getApplicationContext(),MainMenuActivity.class);
+        intent.setClass(getApplicationContext(), MainMenuActivity.class);
         startActivity(intent);
     }
 
     @Override
     public void initializeSharedPrefs() {
-        this.mySharedPrefs = new MySharedPrefs(getApplicationContext(),getResources().getString(R.string.shared_prefs_file_key_card_price_default), Context.MODE_PRIVATE);
+        this.mySharedPrefs = new MySharedPrefs(getApplicationContext(), getResources().getString(R.string.shared_prefs_file_key_card_price_default), Context.MODE_PRIVATE);
     }
-
-
-
-
-
 
     private void setDefaultCardPrice(Integer defaultCardPrice) {
         mySharedPrefs.createOrAccessExistingSharedPreferences();
         mySharedPrefs.writeDefaultCardPriceToSharedPrefs(defaultCardPrice);
     }
 
-
     private void startSetDefaultsActivity() {
         Intent intent = new Intent();
-        intent.setClass(getApplicationContext(),SetDefaultsActivity.class);
+        intent.setClass(getApplicationContext(), SetDefaultsActivity.class);
         startActivity(intent);
     }
 
-
     //SETTERS & GETTERS
-
     //SET
     @Override
     public <T> void setActivityBinding(T DataBindingUtilContent) {
-        this.activityMainBinding =  (ActivityMainBinding) DataBindingUtilContent;
+        this.activityMainBinding = (ActivityMainBinding) DataBindingUtilContent;
     }
 
     //GET
-    final ActivityMainBinding getActivityMainBinding(){
+    final ActivityMainBinding getActivityMainBinding() {
         return this.activityMainBinding;
     }
-
-
 
 }
 
