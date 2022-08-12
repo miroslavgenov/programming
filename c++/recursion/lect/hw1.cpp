285f8df6a4fa3b3a638319954b4205f56ca4c0aa
miroslavgenov <buchvata123@abv.bg>
2022-08-12 20:30:40 +0300

    hw solved

---
+++ c++/recursion/lect/hw1.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+using namespace std;
+
+void rec1(int n){
+    if(n==-1)return;
+    rec1(n-1);
+    cout<<n<<" ";
+}
+void rec2(int n){
+    if(n==-1)return;
+    cout<<n<<" ";
+    rec2(n-1);
+}
+
+void rec3(int n){
+    rec1(n);
+    rec2(--n);
+}
+
+void rec4(int zero,int to){
+    if(zero==to+1)return;
+    
+    cout<<zero<<" ";
+    
+    if(zero<to){
+        rec4(zero+1,to);
+        cout<<zero<<" ";
+    }
+    
+}
+
+void rec5(int n){
+    if(n==-1)return;
+    
+    cout<<n<<" ";
+    
+    if(n!=0){
+        rec5(n-1);
+        cout<<n<<" ";
+    }
+}
+
+
+
+
+int main(){
+    rec1(5);
+    cout<<endl;
+    rec2(5);
+    cout<<endl;
+    rec3(5);
+    cout<<endl;
+    rec4(0,5);
+    cout<<endl;
+    rec5(5);
+    cout<<endl;
+}
