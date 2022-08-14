69ed53de184d72d4f1acb0e7b31965135fd3a601
miroslavgenov <buchvata123@abv.bg>
2022-08-14 19:41:04 +0300

    added animation

--- Android/Quizator/app/src/main/java/com/miroslav/quizator/MainActivity.java
+++ Android/Quizator/app/src/main/java/com/miroslav/quizator/MainActivity.java
@@ -1,11 +1,13 @@
 package com.miroslav.quizator;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.graphics.drawable.AnimationDrawable;
+import android.graphics.drawable.TransitionDrawable;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,7 +16,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN);
+
         Button btn  = findViewById(R.id.button);
+        ConstraintLayout constraintLayout = findViewById(R.id.root_layout);
+        AnimationDrawable animationDrawable = (AnimationDrawable) constraintLayout.getBackground();
+        animationDrawable.setEnterFadeDuration(10);
+        animationDrawable.setExitFadeDuration(5000);
+        animationDrawable.start();
+
+        TransitionDrawable transitionDrawable = (TransitionDrawable) btn.getBackground();
+        btn.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                if(motionEvent.getAction()==MotionEvent.ACTION_DOWN){
+                    transitionDrawable.startTransition(50);
+                }else if(motionEvent.getAction()== MotionEvent.ACTION_UP){
+                    transitionDrawable.reverseTransition(50);
+                }
+                return false;
+            }
+        });
+
+
 
     }
 }
