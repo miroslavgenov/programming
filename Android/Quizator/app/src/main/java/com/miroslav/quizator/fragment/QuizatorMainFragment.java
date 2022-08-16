a9ee067462d536912844d10cc3a89abc2fa572e6
miroslavgenov <buchvata123@abv.bg>
2022-08-16 08:25:24 +0300

    added answers sceme

--- Android/Quizator/app/src/main/java/com/miroslav/quizator/fragment/QuizatorMainFragment.java
+++ Android/Quizator/app/src/main/java/com/miroslav/quizator/fragment/QuizatorMainFragment.java
@@ -1,36 +1,83 @@
 package com.miroslav.quizator.fragment;
 
+import android.icu.number.ScientificNotation;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 
+import android.transition.Explode;
 import android.transition.Scene;
+import android.transition.Transition;
+import android.transition.TransitionManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.miroslav.quizator.Binding;
 import com.miroslav.quizator.R;
+import com.miroslav.quizator.databinding.FragmentQuizatorMainBinding;
 
 
-public class QuizatorMainFragment extends Fragment {
+public class QuizatorMainFragment extends Fragment implements Binding {
+    FragmentQuizatorMainBinding fragmentQuizatorMainBinding;
+    FragmentQuizatorMainBinding dataBindingUtilContent;
+    final int LAYOUT_QUIZATOR_MAIN_FRAGMENT_ID=  R.layout.fragment_quizator_main;
 
 
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+        initFragmentDataBindingUtilContent(inflater,container);
+        setBinding();
 
-        return inflater.inflate(R.layout.fragment_quizator_main, container, false);
+
+        return fragmentQuizatorMainBinding.getRoot();
 
 
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Scene sceneQuestion = Scene.getSceneForLayout(fragmentQuizatorMainBinding.layoutQuestion,R.layout.question1_scene,getContext());
+        sceneQuestion.enter();
+
+        Scene sceneAnswers = Scene.getSceneForLayout(fragmentQuizatorMainBinding.layoutAnswers,R.layout.answers_scene_1,getContext());
+        sceneAnswers.enter();
+
+
+    }
+
+    @Override
+    public void initActivityDataBindingUtilContent() {
+
+    }
+
+    @Override
+    public <T> T getBinding() {
+        return (T) fragmentQuizatorMainBinding;
+    }
+
+    @Override
+    public void initFragmentDataBindingUtilContent(LayoutInflater inflater, ViewGroup container) {
+        dataBindingUtilContent = DataBindingUtil.inflate(inflater,LAYOUT_QUIZATOR_MAIN_FRAGMENT_ID,container,false);
+    }
+
+    @Override
+    public void setBinding() {
+        fragmentQuizatorMainBinding = dataBindingUtilContent;
+    }
+
+
 }
 
 
