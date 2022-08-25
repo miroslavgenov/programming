7788ebecb88fee1ecf8438592077a3f63b5674f1
miroslavgenov <buchvata123@abv.bg>
2022-08-25 17:11:41 +0300

    delete tree node

--- c++/dataStructures/tree/main.cpp
+++ c++/dataStructures/tree/main.cpp
@@ -75,52 +75,106 @@ node* search(node *root, int num){
 	
 }
 
-void dp(int val){
+void dp(node* root, int num){
 	
-	node* t= search(root,val);
-	if(t){
-		cout<<"found"<<endl;
-		if(t->left && t->right){
-			cout<<"have left and right child"<<endl;
-		}else if(t->left && !t->right){
-			cout<<"have only left child"<<endl;
-		}else if(!t->left && t->right){
-			cout<<"have only right child"<<endl;
-		}else if(!t->left && !t->right){
-			cout<<"dont have child"<<endl;
-			cout<<t->num<<endl;
+	if(root){
+		if(root->num == num){
+			cout<<"root is num: "<<num<<" found"<<endl;
+		}else if(root->num > num){
+			cout<<root->num<<" > "<<num<<endl;
+			if(root->left->num == num){
+				cout<<"TARGET FOUND"<<endl;
+				cout<<"root->left == num"<<endl;
+				if(root->left->left && root->left->right){
+					cout<<"root->left have left and right childs"<<endl;
+					cout<<"deleted abadoned"<<endl;
+					cout<<"find max left or min right value"<<endl;
+				}else if(root->left->left && !root->left->right){
+					cout<<"root->left have only left child"<<endl;
+					cout<<"target node is replaces with root->left->left"<<endl;
+					root->left = root->left->left;
+
+				}else if(!root->left->left && root->left->right){
+					cout<<"root->left have only right child"<<endl;
+					cout<<"target node is replacet whith root->left->right"<<endl;
+					root->left = root->left->right;
+//					cout<<"deleted abadoned"<<endl;
+				}else if(!root->left->left && !root->left->right){
+					cout<<"root->left don't have childs"<<endl;
+					cout<<"deleting target left node"<<endl;
+					root->left=nullptr;
+				}
+			}else{
+				cout<<"TARGET IS NOT IN THIS ROOT check in root->left"<<endl;
+				dp(root->left,num);
+			}
+		}else if(root->num < num){
+			cout<<root->num<<" < "<<num<<endl;
+			if(root->right->num == num){
+				cout<<"TARGET FOUND"<<endl;
+				cout<<"root->right == num"<<endl;
+				if(root->right->left && root->right->right){
+					cout<<"root->right have left and righ child"<<endl;
+					cout<<"deleted abadoned"<<endl;
+					
+				}else if(root->right->left && !root->right->right){
+					cout<<"root->right have only left child"<<endl;
+					cout<<"root->right is replaced with root->right->left"<<endl;
+					root->right = root->right->left;
+				}else if(!root->right->left && root->right->right){
+					cout<<"root->right have only right child"<<endl;
+					cout<<"root->right is replaced with root->right->right"<<endl;
+					root->right = root->right->right;
+					
+				}else if(!root->right->right && !root->right->right){
+					cout<<"root->right don't have childs"<<endl;
+					cout<<"deleting targeted node"<<endl;
+					root->right=nullptr;
+				}
+			}else{
+				cout<<"TARGET IS NOT IN THIS ROOT check in root->right"<<endl;
+				dp(root->right, num);
+			}
+			
 			
 		}
-	}else{
-		cout<<"not found"<<endl;
 	}
-	
 
 }
 
 int main(){
 	node *n1 = new node{3, nullptr, nullptr};	
-	node *n2 = new node{5, nullptr, nullptr};
-	node *n3 = new node{1, nullptr, nullptr};
-	node *n4 = new node{2, nullptr, nullptr};
-	node* n5 = new node{6, nullptr, nullptr};
-	node* n6 = new node{7, nullptr, nullptr};
+	node *n2 = new node{2, nullptr, nullptr};
+	node *n3 = new node{4, nullptr, nullptr};
+	node *n4 = new node{1, nullptr, nullptr};
+	node *n5 = new node{0, nullptr, nullptr};
+	node *n6 = new node{7, nullptr,nullptr};
+	node *n7 = new node{8, nullptr,nullptr};
+	node *n8 = new node{10, nullptr,nullptr};
+	node *n9 = new node{9, nullptr,nullptr};
 	
-	init(4);
-	append(root, n2);
+	init(5);
 	append(root, n1);
+	append(root, n2);
 	append(root, n3);
 	append(root, n4);
 	append(root, n5);
 	append(root, n6);
-	dp(2);
+	append(root, n7);
+	append(root, n8);
+	append(root, n9);
+	
+	dp(root,5);
 	print(root);
 	
 	
 	
-//	search(root, 7);
-//	dp(1);
 	
+	
+	
+	
+	
+
 	
 }
 
