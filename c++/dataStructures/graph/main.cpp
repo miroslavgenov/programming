e928d05747138cd55fa99c556ce39aa7e338e423
miroslavgenov <buchvata123@abv.bg>
2022-08-27 19:56:24 +0300

    random graph print

--- c++/dataStructures/graph/main.cpp
+++ c++/dataStructures/graph/main.cpp
@@ -1,9 +1,36 @@
 #include <iostream>
 
+#include <vector>
+
+
 using namespace std;
 
+bool isNumberInArray(vector<int> v, int n){
+	if(v.size()==0){
+		return 0;	
+	}else{
+		for(int i=0;i<v.size();i++){
+		if(n == v[i]){
+			return 1;
+		}
+	}	
+	}
+	
+	return 0;
+}
+
+template <typename T>
+void printV(vector<T>v){
+	for(int i=0;i<v.size();i++)
+		cout<<v[i]<<" ";
+}
+const int s=5;
+void printGraphEdgeArc(int [][s]);
 int main(){
-	int s=5;
+	vector<int> v;
+	v.push_back(0);
+	
+	
 	int g[s][s]={
 		{0,1,0,0,1},
 		{1,0,1,1,1},
@@ -13,17 +40,34 @@ int main(){
 	};
 	
 	
-	for(int i=0; i<s; i++){
-	cout<<"edge: "<<i<<" have connection with: ";	
-		for(int j=0; j<s; j++){
-			if(g[i][j] == 1){
-				cout<<j<<' ';	
+	for(int i=0;i<s;i++){
+		for(int j=0;j<s;j++){
+			if(g[i][j] == 1)  {
+				if(!isNumberInArray(v,j)){
+					v.push_back(j);
+				}		
 			}
-			
-
-		}cout<<endl;
+		}
 	}
 	
+	printV(v);
+	printGraphEdgeArc(g);
+	
+
+	
+	
+	
+}
+void printGraphEdgeArc(int g[][s]){
+	cout<<endl;
+	for(int i=0;i<s;i++){
+		cout<<"edge: "<<i<<" arc: ";
+		for(int j=0;j<s;j++){
+			if(g[i][j] == 1){
+				cout<<j<<" ";
+			}
+		}cout<<endl;
+	}
 }
 
 
