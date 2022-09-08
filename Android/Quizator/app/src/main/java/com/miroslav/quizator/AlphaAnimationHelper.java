4ff9eb097508d13fa485f5492683fe03113ce027
miroslavgenov <buchvata123@abv.bg>
2022-09-08 14:27:41 +0300

    refactor animationhelper

--- Android/Quizator/app/src/main/java/com/miroslav/quizator/AlphaAnimationHelper.java
+++ Android/Quizator/app/src/main/java/com/miroslav/quizator/AlphaAnimationHelper.java
@@ -2,59 +2,17 @@ package com.miroslav.quizator;
 
 import android.view.View;
 import android.view.animation.AlphaAnimation;
-import android.view.animation.Animation;
-import android.widget.LinearLayout;
-import android.widget.TextView;
-
-import com.miroslav.quizator.actor.Question;
-import com.miroslav.quizator.databinding.FragmentQuizatorMainBinding;
 
 public class AlphaAnimationHelper {
-    LinearLayout layoutQuestionRoot;
-    LinearLayout layoutAnswersRoot;
-    AlphaAnimation fadeInForQuestionAndAnswersRootLayout;
-    AlphaAnimation fadeOutForQuestionAndAnswersRootLayout;
-    FragmentQuizatorMainBinding fragmentQuizatorMainBinding;
-    Question question;
-    final int ANIMATION_DURATION = 1000;
-
-    
-
+	// TODO remove this constructur and replace it with static
     public AlphaAnimationHelper(View targetViewToStartAnimation, int fromAlpha, int toAlpha, int animationDuration) {
-        targetViewToStartAnimation.startAnimation(createAlphaAnimation(fromAlpha,toAlpha,animationDuration));
-    }
-
-    public AlphaAnimationHelper(View layoutQuestionRoot, View layoutAnswersRoot,
-            FragmentQuizatorMainBinding fragmentQuizatorMainBinding ) {
-        this.layoutQuestionRoot = (LinearLayout) layoutQuestionRoot;
-        this.layoutAnswersRoot = (LinearLayout) layoutAnswersRoot;
-        this.fragmentQuizatorMainBinding = fragmentQuizatorMainBinding;
-        fadeInForQuestionAndAnswersRootLayout = createAlphaAnimation(1,0,ANIMATION_DURATION);
-        fadeOutForQuestionAndAnswersRootLayout = createAlphaAnimation(0,1,ANIMATION_DURATION/2);
+        targetViewToStartAnimation.startAnimation(createAlphaAnimation(fromAlpha, toAlpha, animationDuration));
     }
 
     public static AlphaAnimation createAlphaAnimation(int fromAlpha, int toAlpha, int animationDuration) {
-        AlphaAnimation alphaAnimation = new AlphaAnimation(fromAlpha,toAlpha);
+        AlphaAnimation alphaAnimation = new AlphaAnimation(fromAlpha, toAlpha);
         alphaAnimation.setDuration(animationDuration);
         return alphaAnimation;
-    }
-
-    public void setFragmentQuizatorMainBinding(FragmentQuizatorMainBinding fragmentQuizatorMainBinding) {
-        this.fragmentQuizatorMainBinding = fragmentQuizatorMainBinding;
-    }
-
-    public void setQuestionForAnimation(Question question) {
-        this.question = question;
-    }
-
-    public Question getQuestion() {
-        return question;
-    }
-
-     public void startAnimation() {
-        layoutQuestionRoot.startAnimation(fadeInForQuestionAndAnswersRootLayout);
-        layoutAnswersRoot.startAnimation(fadeInForQuestionAndAnswersRootLayout);
-
     }
 
 }
