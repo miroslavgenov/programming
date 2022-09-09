8c6cea78a46d6b7f5cf085f2545418b3569bf9e0
miroslavgenov <buchvata123@abv.bg>
2022-09-09 19:53:19 +0300

    egyptianFraction not my aproach

--- c++/greedyAlgorithms/egymptianFraction/main.cpp
+++ c++/greedyAlgorithms/egymptianFraction/main.cpp
@@ -1,13 +1,67 @@
+/**
+ 	!! not my aproach !!
+ 
+ */
+
+
 #include <iostream>
 #include <vector>
 
 using namespace std;
-struct fraction{
-	int numerator;
-	int denominator;
+
+struct Fraction{
+	int p;
+	int q;
 };
 
+Fraction* createMaximumFractionFrom(Fraction *currentFraction){
+	int maximumDenominator = ((double)(currentFraction->q+currentFraction->p)/(double)currentFraction->p);
+	
+	return new Fraction{1, maximumDenominator};
+}
+
+void makeNewFractionUsingCurrentFractionAndMaxFration(Fraction* currentFraction, Fraction *maximumFraction){
+	
+	currentFraction->p = currentFraction->p * maximumFraction->q - currentFraction->q;
+	currentFraction->q = currentFraction->q * maximumFraction->q;
+		
+	if(currentFraction->q % currentFraction->p == 0){
+		currentFraction->q = currentFraction->q / currentFraction->p;
+		currentFraction->p = 1;
+	}
+	
+}
+
+vector<Fraction*>usedFractions;
+
+bool isNumeratorOfCurrentFractionEqualsToOne(Fraction* currentFraction){
+	return currentFraction->p == 1;
+}
+
+vector<Fraction*> findEgyptianFraction(Fraction* currentFraction){
+	Fraction* maxFraction=nullptr;
+
+	while(!isNumeratorOfCurrentFractionEqualsToOne(currentFraction)){
+		maxFraction = createMaximumFractionFrom(currentFraction);
+		usedFractions.push_back(maxFraction);
+		makeNewFractionUsingCurrentFractionAndMaxFration(currentFraction,maxFraction);
+	}
+
+	usedFractions.push_back(currentFraction);
+
+	return usedFractions;
+}
 
 int main(){	
-	fraction number1 = {7,9};
+	Fraction* currentFraction = new Fraction{12, 13};
+	
+	findEgyptianFraction(currentFraction);
+	
+	for(int i=0;i<usedFractions.size();i++){
+		cout<<usedFractions[i]->p<<" "<<usedFractions[i]->q<<endl;
+	}
+	
+
+
+	
 }
