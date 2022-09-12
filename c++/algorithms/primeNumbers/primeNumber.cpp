be63a9b7e5f5e70236805607068709fc57d7fdcd
miroslavgenov <buchvata123@abv.bg>
2022-09-12 21:01:55 +0300

    refactor primeNumber

---
+++ c++/algorithms/primeNumbers/primeNumber.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+
+const int NUMBER_TWO = 2;
+
+bool isNumberDividable(int source_number);
+bool isNumberGreatherThanTwo(int *source_number);
+bool isNumberNullptr(int *source_number);
+bool isNumberNotNullAndGreaterThanTwo(int *source_number);
+bool isNumberPrime(int *source_number);
+bool isTheNumberSuitableToContinue(int *number);
+
+
+int main(){	
+	int *number=new int(11);
+	std::cout<<isNumberPrime(number);
+}
+
+bool isNumberPrime(int *source_number){
+	return isNumberNotNullAndGreaterThanTwo(source_number) && !isNumberDividable(*source_number);
+}
+
+bool isNumberNotNullAndGreaterThanTwo(int *source_number){
+	return isTheNumberSuitableToContinue(source_number);
+}
+
+bool isTheNumberSuitableToContinue(int *number){
+	return !isNumberNullptr(number) && isNumberGreatherThanTwo(number);
+}
+
+bool isNumberNullptr(int *source_number){
+	return source_number == nullptr;
+}
+
+bool isNumberGreatherThanTwo(int *source_number){
+	return *source_number > NUMBER_TWO;
+}
+
+bool isNumberDividable(int source_number){
+	for(int i = 2; i < source_number; i++){
+		if(source_number%i == 0){
+			 return true;	
+		}
+	}
+	return false;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
