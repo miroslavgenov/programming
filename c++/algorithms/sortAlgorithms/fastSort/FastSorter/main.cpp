42fd57cb2994e7775f1cd6b82e3c5e2b1573b710
miroslavgenov <buchvata123@abv.bg>
2022-09-23 15:14:29 +0300

    fastSorter

--- c++/algorithms/sortAlgorithms/fastSort/FastSorter/main.cpp
+++ c++/algorithms/sortAlgorithms/fastSort/FastSorter/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "/home/home/Desktop/programming/c++/algorithms/sortAlgorithms/NumberSorter.cpp"
+#include <vector>
 #include <ctime>
 #include <cstdlib>
 
@@ -8,71 +9,110 @@ using namespace std;
 size_t numbersSize = 10;
 
 void print(int a[], int s){
-    cout<<endl;
     for(int i=0;i<s;i++){
         cout<<a[i]<<' ';
     }
     cout<<endl;
 }
-void fs(int l,int r ,int numbers[]){
-    if(l<r){
 
-    
-    int left = l;
+    vector<int> lesserNumbers;
+    vector<int> greaterNumbers;
+
+bool isLeftSotered = false;
+
+void print(int numbers[],int from, int size){
+    for(int i=from;i<size;i++){
+        cout<<numbers[i]<<" ";
+    }cout<<endl;
+}
+
+void fs(int l,int r ,int numbers[]){
+    lesserNumbers.clear();
+    greaterNumbers.clear();
+    int left =l;
     int right = r;
     int mid = (left+right)/2;
-    // cout<<left<<" "<<mid<<" "<<right<<endl;
-    // print(numbers,r+1);
-
-    // cout<<left<<" "<<mid<<" "<<right<<endl;
-    // cout<<numbers[left]<<" "<<numbers[mid]<<" "<<numbers[right]<<endl;
-
-    int i=left;
-    int j = right;
+    int midNumberValue = numbers[mid];
+    // cout<<"leftIdx: "<<left<<" midIdx: "<<mid<<" rightIdx: "<<right<<endl;
+    
+    int i=left,j=right;
+    // do{
+        while(numbers[i]<midNumberValue){
+            i++;
+        }
+        while(numbers[j]>midNumberValue){
+            j--;
+        }
+        cout<<numbers[i]<<" "<<numbers[mid]<<" "<<numbers[j]<<endl;
+        // NumberSwapper::swapTwoNumbersValue(&numbers[i],numbers[j]);
+        int temp = numbers[i];
+        numbers[i] = numbers[j];
+        numbers[j] = temp;
+    // }while(i<j);
     
 
-    do{
-    while(numbers[i] < numbers[mid]){
-        i++;
-    }
-    
-    while(numbers[j]>numbers[mid]){
-        j--;
-    }
-    NumberSwapper::swapTwoNumbersValue(&numbers[i],&numbers[j]);
-    
-    }
-    while(i<j);
-    // print(numbers,numbersSize);
-    // cout<<left<<" "<<mid<<" "<<right<<endl;
-    
-    
-    
-    
+    // move lesser numbers than mid value in lesserNumbers and greater in greaterNumbers
+    // for(int i=0;i<right;i++){
+    //     if(numbers[i]<numbers[mid]){
+    //         lesserNumbers.push_back(numbers[i]);
+    //     }
+    //     else if(numbers[i]> numbers[mid]){
+    //         if(mid != i){
+    //             greaterNumbers.push_back(numbers[i]);
+    //         }
+            
+    //     }
+    // }
 
-    }else{
-        return;
-    }
+
+    // update the new array
+    // for(int i=0;i<lesserNumbers.size();i++){
+    
+    //     numbers[i] = lesserNumbers[i];
+    // }
+    
+    // numbers[lesserNumbers.size()] = midNumberValue;
+    
+    // for(int i=0, j = lesserNumbers.size()+1;i<greaterNumbers.size();i++,j++){
+    //     numbers[j] = greaterNumbers[i];
+    // }
+    // cout<<l<<" "<<r<<endl;
+    // print(numbers,left,right);
+
+    // if(left < right){
+        // fs(left,(left+right)/2,numbers);
+    // }
+    
+    // if(mid < r-1){
+        // fs(mid,r,numbers);
+    // }    
 }
 
 int main(){  
     srand(time(0));
 
     
-    int numbers[numbersSize];
+    int numbers[numbersSize] = {25,35,81,83,99,75,96,39,1,28};
     
-    for(int i=0; i<numbersSize;i++){
-        numbers[i] = rand()% 100;
-    }
+    // for(int i=0; i<numbersSize;i++){
+    //     numbers[i] = rand()% 100;
+    // }
+    
     print(numbers,numbersSize);
-    fs(0,numbersSize-1,numbers);
-print(numbers,numbersSize);
+    fs(0,numbersSize,numbers);
+    print(numbers,numbersSize);
     
+    // if(ns == 1){
+    //     cout<<"ns == 1 can't compare only one number.Left part is done"<<endl; 
+    // }
     
 
+    // returning
     
 
-    // print(numbers,numbersSize);
+
+    
+    
 
     
 
