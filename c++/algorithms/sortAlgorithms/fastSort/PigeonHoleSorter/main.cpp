e1d06376705407a6b0e637639444a6182abdaed6
miroslavgenov <buchvata123@abv.bg>
2022-09-28 22:12:03 +0300

    added NumberFinder

--- c++/algorithms/sortAlgorithms/fastSort/PigeonHoleSorter/main.cpp
+++ c++/algorithms/sortAlgorithms/fastSort/PigeonHoleSorter/main.cpp
@@ -3,15 +3,13 @@
 #include <ctime>
 #include <cstdlib>
 #include "/home/home/Desktop/programming/c++/algorithms/sortAlgorithms/NumberSorter.cpp"
+#include "/home/home/Desktop/programming/c++/algorithms/searchAlgorithms/NumberFinder.cpp"
+
 
 using namespace std;
 
-void print(int numbers[], int numbersSize){
-	for(int i=0;i<numbersSize;i++){
-	cout<<numbers[i]<<" ";
-	}cout<<endl;
-}
 class PigeonHoleSorter : NumberSorter{
+	private:
 	vector<vector<int>> pigeonHoles;
 	int minimumNumber;
 	int maximumNumber;
@@ -34,33 +32,14 @@ public:
 
 	
 	
+	
+private:
+
 	void shouldSwapTheGreaterNumberWithLesserNumber(int* greater, int *lesser){
 
-	}
-
-	int findMinimumNumberFromArray(){
-		minimumNumber = numbers[0];
-		
-		for(int i =0 ;i<numbers.size();i++){
-			if(minimumNumber > numbers[i]){
-				minimumNumber = numbers[i];
-			}
-		}
-		return minimumNumber;
-	}
-
-	int findMaximumNumberFromArray(){
-		maximumNumber = numbers[0];
-		
-		for(int i=0;i<numbers.size();i++){
-			if(maximumNumber < numbers[i]){
-				maximumNumber = numbers[i];
-			}
-		}
-		return maximumNumber;
-	}
+	}	
 	
-
+public:
 	void sort(){
 		for(int i=0;i<numbers.size();i++){
 			pigeonHoles[getTheSuitablePigeonHoleIndexForTheCurrentNumber(numbers[i])].push_back(numbers[i]);
@@ -76,7 +55,7 @@ public:
 			}
 		}
 	}
-
+private:
 	int getTheSuitablePigeonHoleIndexForTheCurrentNumber(int currentNumber){
 		return currentNumber - getMinimumNumber();
 	}
@@ -85,9 +64,15 @@ public:
 		return minimumNumber;
 	}
 
-	void setMaximumNumber(){maximumNumber = findMaximumNumberFromArray();}
-	void setMinimumNumber(){minimumNumber = findMinimumNumberFromArray();}
+	void setMaximumNumber(){
+		maximumNumber = NumberFinder::findMaximumNumberFromArray(numbers);
+	}
 	
+	void setMinimumNumber(){
+		minimumNumber = NumberFinder::findMinimumNumberFromArray(numbers);
+	}
+	
+public:
 	void printNumbers(){
 		NumberSorter::printNumbers();
 	}
@@ -97,17 +82,25 @@ public:
 int main(){
 	srand(time(0));
 		
-	int numbersSize = 2094901;
-	int numbers[numbersSize];// = {3, 11, 14, 10, 11, 11, 10, 2, 4, 8};
+	int numbersSize = 10; // = 2094901;
+	int numbers[numbersSize] = {3, 11, 14, 10, 11, 11, 10, 2, 4, 8};
+	// vector<int>  a ;
 	
-	for(int i=0;i<numbersSize;i++){
-		numbers[i] = rand()% 10001;
-	}
+	// for(int i=0;i<numbersSize;i++){
+	// 	a.push_back(numbers[i]);
+	// }
 
-	PigeonHoleSorter *p = new PigeonHoleSorter(numbers,numbersSize);
-	p->printNumbers();
-	p->sort();
-	p->printNumbers();
+	// cout<<NumberFinder::findMaximumNumberFromArray(a)<<endl;
+	// cout<<NumberFinder::findMaximumNumberFromArray(numbers,numbersSize)<<endl;
+
+	// for(int i=0;i<numbersSize;i++){
+	// 	numbers[i] = rand()% 10001;
+	// }
+
+	// PigeonHoleSorter *p = new PigeonHoleSorter(numbers,numbersSize);
+	// p->printNumbers();
+	// p->sort();
+	// p->printNumbers();
 	
 	
 
