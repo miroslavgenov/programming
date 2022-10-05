d81914dc2cffee3f39b1448713998c08028c1281
miroslavgenov <buchvata123@abv.bg>
2022-10-05 18:31:36 +0300

    BinarySearcher refactor

--- c++/algorithms/searchAlgorithms/BinarySearcher/main.cpp
+++ c++/algorithms/searchAlgorithms/BinarySearcher/main.cpp
@@ -1,61 +1,49 @@
 #include <iostream>
 #include <vector>
+#include "/home/home/Desktop/programming/c++/algorithms/ArrayChecker.cpp"
+#include "/home/home/Desktop/programming/c++/algorithms/searchAlgorithms/NumberSearcher.h"
 
 using namespace std;
 
-class ArrayChecker{
-public:
-	template <typename T>
-	static bool isArraySorted(T array[], int size){
-		for(int i=1;i<size;i++){
-			if(array[i-1] > array[i]){
-				return false;
-			}
-		}
-		return true;
-	}
-};
-
-
-class BinarySearcher{
-	int numberToBeFound;
-	vector<int> numbers;
+class BinarySearcher: NumberSearcher{
 	int left = 0;
 	int right;
 	int mid;
 
 public:
-	BinarySearcher(int numbers[], size_t numbersSize, int numberToBeFound){
-		if(!ArrayChecker::isArraySorted(numbers,numbersSize)){
+	
+	BinarySearcher(int numbers[], size_t numbersSize, int findThisNumber):NumberSearcher(numbers, numbersSize, findThisNumber){
+			right = numbersSize-1;
+			mid = (left+right)/2;
+	}
+
+	int search(){
+		if(ArrayChecker::areNumbersSorted(numbers,numbersSize)){
+			 binarySearchIndexOfFindThisNumber(left,mid,right,findThisNumber);	
+			 return foundIndex;
+		}
+		else{
 			cout<<"sort your first array"<<endl;
 		}
-		this->numberToBeFound = numberToBeFound;
-		for(int i = 0;i<numbersSize;i++){
-			this->numbers.push_back(numbers[i]);
-		}
-		
-		right = this->numbers.size()-1;
-		mid = (left+right)/2;
 
+		return indexIsNotFound;
 	}
-
-	void search(){
-		binarySearch(left,mid,right,numberToBeFound);
-	}
+	
 private:
-	void binarySearch(int left, int mid, int right, int toBeFound){
-	if(numbers[mid] == toBeFound){
-		cout<<"number: "<<toBeFound<<" is at index: "<<mid<<endl;
+	void binarySearchIndexOfFindThisNumber(int left, int mid, int right, int findThisNumber){
+	if(numbers[mid] == findThisNumber){
+		// cout<<"number: "<<findThisNumber<<" is at index: "<<mid<<endl;
+		foundIndex = mid;
 		return ;
 	}
 	if(left >= right){
-		cout<<toBeFound<<" is not found"<<endl;
+		// cout<<findThisNumber<<" is not found"<<endl;
 		return;
 	}
-	if(numbers[mid] < toBeFound){
-		binarySearch(mid + 1, (mid+1 + right) / 2, right, toBeFound);
+	if(numbers[mid] < findThisNumber){
+		binarySearchIndexOfFindThisNumber(mid + 1, (mid+1 + right) / 2, right, findThisNumber);
 	}else{
-		binarySearch(left, ((mid - 1) + left) / 2, mid - 1, toBeFound);
+		binarySearchIndexOfFindThisNumber(left, ((mid - 1) + left) / 2, mid - 1, findThisNumber);
 	}
 }
 };
@@ -67,7 +55,7 @@ int main(){
 	int numbers[numbersSize] = {1,2,3,4,5,6,7,8,9,10};
 	
 	BinarySearcher * b = new BinarySearcher(numbers,numbersSize,3);
-	b->search();
+	cout<<numbers[b->search()];
 
 	
 
