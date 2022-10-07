b9094b4570ddcd249afcbf92b8501f21cd6c148a
miroslavgenov <buchvata123@abv.bg>
2022-10-07 19:03:37 +0300

    Added more functions in LinkedList

--- c++/dataStructures/LinkedList/main.cpp
+++ c++/dataStructures/LinkedList/main.cpp
@@ -5,6 +5,7 @@ using namespace std;
 template<typename T>
 class LinkedListSingle{
 public:
+	const int EMPTY_SIZE_LENGTH = 0;
 	static int totalListElements;
 	T data;
 	LinkedListSingle* root = nullptr;
@@ -14,7 +15,7 @@ public:
 	LinkedListSingle(T data){
 		totalListElements++;
 
-		if(totalListElements == 0){
+		if(totalListElements == EMPTY_SIZE_LENGTH){
 			root = this;
 		}
 		
@@ -29,30 +30,50 @@ public:
 		delete root;
 	}
 
+
+
 	void insertBeforeRoot(T data){
-		LinkedListSingle* currentRoot = new LinkedListSingle(this->data);
-		if(this->next){
-			currentRoot->next = this->next;	
+		LinkedListSingle* currentRoot = nullptr;
+		if(this->totalListElements == this->EMPTY_SIZE_LENGTH){
+			totalListElements++;
+			this->data = data;
+		}else{
+			
+			currentRoot = new LinkedListSingle(this->data);
+			
+			// if current root object have next copy the next cuz we make only constructor with T data
+			if(this->next){
+				currentRoot->next = this->next;
+			}
+
+			this->data = data;
+			this->next = currentRoot;
 		}
-		
-		this->data = data;
-		this->next = currentRoot;	
 	}
 
 	void insert(T data){
-		LinkedListSingle* linkedListIterator = this;
+		LinkedListSingle* linkedListIterator = nullptr;
 		
-		// go to the last element from the list
-		while(linkedListIterator->next){
-			linkedListIterator=linkedListIterator->next;
-		}
+		if(this->totalListElements == this->EMPTY_SIZE_LENGTH){
+			this->data = data;
+			totalListElements++;	
+		}else{
 
-		linkedListIterator->next = new LinkedListSingle(data);
+			linkedListIterator = this;
+
+			// go to the last element from the list
+			while(linkedListIterator->next){
+				linkedListIterator = linkedListIterator->next;
+			}
+
+			linkedListIterator->next = new LinkedListSingle(data);	
+		}
 	}
 
 	void deleteFromEnd(){
 		LinkedListSingle* iterator = this;
-		if(iterator->next){
+
+		if(totalListElements > 1){
 			
 			// go before before the end
 			while(iterator->next->next){
@@ -62,15 +83,75 @@ public:
 			// null the pointer that is pointing to the end
 			iterator->next = nullptr;	
 
+			this->totalListElements--;
+
 		}else{
+			cout<<"delete from end"<<endl;
 			totalListElements = 0;
 		}
-		
+	}
+
+	void deleteFromBegining(){
+		if(totalListElements > EMPTY_SIZE_LENGTH){
+			if(this->next){
+				this->data = this->next->data;
+				this->next = this->next->next;
+				totalListElements--;
+			}else{
+				totalListElements = 0;
+			}
+
+		}
+	}
+
+	void deleteAtIndex(int targetIndex){
+		// do we have elements
+		if(totalListElements > EMPTY_SIZE_LENGTH){
+
+			// is targetIndex in range
+			if(targetIndex <= totalListElements - 1 && targetIndex >= EMPTY_SIZE_LENGTH){
+
+				// use already defined methods if targetIndex is: 0, totalElements-1 , mid
+				// use special method if not
+				if(targetIndex == EMPTY_SIZE_LENGTH){
+					deleteFromBegining();
+				}else if(targetIndex == totalListElements - 1){
+					deleteFromEnd();
+				}else if(targetIndex == totalListElements / 2){
+					deleteFromMid();
+				}
+				// else{}  //custom logic
+
+			}
+
+
+		}
+
 	}
 
 	void deleteFromMid(){
-		int mid = totalListElements/2;
+		LinkedListSingle* iterator = this;
+
+		if(totalListElements == 1){
+			deleteFromEnd();
+
+		}else if(totalListElements > 1){
+			int mid = totalListElements / 2;
+			
+
 		
+			for(int i = 0; i < mid - 1; i++){
+				iterator = iterator->next;
+			}
+
+			if(iterator->next->next){
+				iterator->next = iterator->next->next;
+				totalListElements--;
+			}else if(!iterator->next->next){
+				iterator->next = nullptr;
+				totalListElements--;
+			}	
+		}		
 	}
 
 	
@@ -81,13 +162,12 @@ public:
 template <typename X>
 ostream& operator<<(ostream& os, LinkedListSingle<X>* currentRoot){
 	LinkedListSingle<X>* iterator = currentRoot;
-	if(currentRoot->totalListElements>0){
+	if(currentRoot->totalListElements > currentRoot->EMPTY_SIZE_LENGTH){
 		while(iterator){
-		os<<iterator->data<<" ";
-		iterator = iterator->next;
+			os<<iterator->data<<" ";
+			iterator = iterator->next;
+		}
 	}
-	}
-	
 	return os;
 }
 
@@ -98,9 +178,40 @@ int LinkedListSingle<T>::totalListElements= 0;
 
 int main(){
 	LinkedListSingle<int>* l = new LinkedListSingle(1);
-	l->insert(3);
 	
-	l->deleteFromEnd();
+	l->insert(2);
+	l->insert(3);
+	cout<<l<<endl;	
+	
+	
+	
+
+	// cout<<l<<endl;
+	// l->deleteFromMid();
+	// l->deleteFromMid();
+	// l->deleteFromMid();
+	// l->deleteFromMid();
+	// l->deleteAtIndex(2);
+	// l->deleteFromBegining();
+	// l->deleteFromBegining();
+	// l->deleteFromBegining();
+	// l->deleteFromBegining();
+	
+	// l->deleteFromMid();
+	// l->deleteFromMid();
+	// l->deleteFromMid();
+	// l->deleteFromMid();
+	// l->deleteFromMid();
+	// l->deleteFromMid();
+	// l->deleteFromMid();
+	
+	// cout<<"total elements after: "<<l->totalListElements<<endl;
+	
+	// cout<<l<<endl;
+	
+
+	// l->insert(5);
+	// cout<<l<<" "<<l->totalListElements<<endl;
 	
 	
 	
