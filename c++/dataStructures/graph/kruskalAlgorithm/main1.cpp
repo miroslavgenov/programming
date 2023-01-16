1e103939e53afae4e07edcb5f19ed49d6d7425cd
miroslavgenov <buchvata123@abv.bg>
2023-01-16 17:08:18 +0200

    kruskal algorithm solved

--- c++/dataStructures/graph/kruskalAlgorithm/main1.cpp
+++ c++/dataStructures/graph/kruskalAlgorithm/main1.cpp
@@ -45,13 +45,13 @@ vertexConnection* findTheMinimumConnection(int **graph, int graphSize){
     return vt;
 }
 
-bool isVertexVisited(int vertexNumber){
-    return visitedVerticies[vertexNumber] == GraphFlags::visited;
-}
+// bool isVertexVisited(int vertexNumber){
+//     return visitedVerticies[vertexNumber] == GraphFlags::visited;
+// }
 
-bool isBothVerticiesVisited(int firstVertex, int secondVertex){
-    return isVertexVisited(firstVertex) && isVertexVisited(secondVertex);
-}
+// bool isBothVerticiesVisited(int firstVertex, int secondVertex){
+//     return isVertexVisited(firstVertex) && isVertexVisited(secondVertex);
+// }
 
 
 // if they are not visited mark them as visited
@@ -116,36 +116,39 @@ void print(int** graph, int graphSize){
 
 
 vector<int> stack;
-vector<int> loopS;
 const int graphSize = 4;
-bool vV[graphSize]{};
+bool vv[graphSize]{};
+
+template <typename T>
+void print(T array[], int size){
+    for(int i=0;i<size;i++){
+        cout<<array[i]<<" ";
+    }
+}
 
 void dfs(int **graph){
-    while(stack.empty() == false){
-        int cv = stack.back();
-        stack.pop_back();
-
+    if(stack.empty() == false && vv[stack.back()] == 0) {
         
-        cout<<cv<<endl;
+        int currentVertex = stack.back();
+        stack.pop_back();
+        vv[currentVertex] = 1;
+        // cout<<currentVertex<<endl;
+        
+        for(int i=0;i<graphSize;i++){
+            if(graph[currentVertex][i] != 0){
+                if(vv[i]==0){
 
-            for(int i=0;i<graphSize;i++){
-                if(graph[cv][i] !=0){
-                    if(vV[i] == 0){
-                        //no loop
-                        vV[i] = 1;
-                        stack.push_back(i);
-                        cout<<"no loop from: "<<cv<<" to: "<<i<<endl;
-                        loopS.push_back(i);
-                    }else{
-                        //loop
-                        // cout<<"from: "<<cv<<" to: "<<i<<" there will be a loop"<<endl;
-                    }
+                    stack.push_back(i);
                 }
             }
-            
         }
 
         
+
+        if(stack.empty() == false){
+            dfs(graph);
+        }
+    }
     
 }
 
@@ -158,93 +161,12 @@ bool isEveryVertexVisited(){
     return true;
 }
 
-void kruskal(int **graph, int graphSize, int **newGraph){
-    
-    /**
-     * is every vertex visited
-     * no 
-     * find the minimum vertex to vertex connection
-     * check if you add it to the new graph there will be a loop
-     * store the verticies that doesn't make a loop
-     * if there is no loop then add it to the graph
-     * continue until all verticies are visited
-     * yes end
-     * 
-     */
-    // if(isEveryVertexVisited() == false){
-    //     cout<<"kruskal"<<endl;
 
-    //     vertexConnection* vt = findTheMinimumConnection(graph,graphSize);
-    //     cout<<vt->connectionFromVertex<<" "<<vt->connectionToVertex<<" "<<vt->connectionWeight<<endl;
-
-    //     addTheConnectionsToTheNewGraph(newGraph,vt);
-        
-    //     for(int i=0;i<graphSize;i++){
-    //     for(int j=0;j<graphSize;j++){
-    //         if(newGraph[i][j] !=0){
-    //             stack.push_back(j);
-    //             cout<<j<<endl;
-    //             break;
-    //         }
-    //     }
-    //         break;
-    // }
-
-    // // vV[stack.back()] = 1;
-    // // dfs(newGraph);
-        
-    //     dfs(newGraph);
-    //     for(int i: loopS){
-    //         cout<<i<<" ";
-    //     }
-    //     cout<<endl;
-    //     // check for loop
-    // }
-
-    
-}
 
 
 //TO DO: make algorithm that check for loops
 
-
-
-int main(){
-    int graphCp[graphSize][graphSize] = {
-       //0 1 2 3
-        {0,1,0,0}, //0
-        {1,0,2,3}, //1
-        {0,2,0,2}, //2
-        {0,3,0,0}  //3
-	};    
-    
-    
-    
-
-    int **graph = new int*[graphSize];
-    // make this function
-    for(int i=0; i<graphSize;i++){
-            graph[i] = new int[graphSize];
-    }
-    // make this function
-    for(int i=0;i<graphSize;i++){
-        for(int j=0;j<graphSize;j++){
-            graph[i][j] = graphCp[i][j];
-            // cout<<graph[i][j]<<" ";
-        }
-    }
-
-    int **newGraph = new int*[graphSize];
-    for(int i=0;i<graphSize;i++){
-        newGraph[i] = new int[graphSize];
-    }
-
-    // dfs(graph);
-    // kruskal(graph, graphSize, newGraph);
-
-    //  only store verticies that have more than one connection
-    // 
-
+bool isThereACycle(int **graph, int graphSize){
 
     //getting the connection in to a stack
     vector<vertexConnections*> listWithVertexConnections;
@@ -276,19 +198,18 @@ int main(){
     // if simmilar more thatn 6 than there is loop
 
 
-
     //loop detector
     int repeatedConnections = 0;
 
     // loop each vertex
     for(int i=0;i<potentialLoopVerticies.size();i++){
-        cout<<"current vertex: "<<potentialLoopVerticies[i]->vertexNumber<<endl;
+        // cout<<"current vertex: "<<potentialLoopVerticies[i]->vertexNumber<<endl;
         
         // loop each vertex connections
         // loop and see if the connection from the current vertex occur in the other
         for(int j=0;j<potentialLoopVerticies[i]->connections.size();j++){
-            cout<<"connection: "<<potentialLoopVerticies[i]->connections[j]<<endl; // connection from the current vertex
-            cout<<"checking if that connection occur in the other verticies"<<endl;
+            // cout<<"connection: "<<potentialLoopVerticies[i]->connections[j]<<endl; // connection from the current vertex
+            // cout<<"checking if that connection occur in the other verticies"<<endl;
 
             //loop all verticies
             // loop throug all verticies except the current vertex to check for similar connections
@@ -296,36 +217,122 @@ int main(){
                 //show all verticies except the current one
                 // all other verticies except the current vertex
                 if(potentialLoopVerticies[i]->vertexNumber != potentialLoopVerticies[z]->vertexNumber){
-                    cout<<"vertex: "<<potentialLoopVerticies[z]->vertexNumber<<" : ";
+                    // cout<<"vertex: "<<potentialLoopVerticies[z]->vertexNumber<<" : ";
 
                     //loop the connections from the verticies and check for similar
                     //looping throught the other verticies connections and checking for repeted connections
                     for(int y =0 ; y< potentialLoopVerticies[z]->connections.size();y++){
-                        cout<<potentialLoopVerticies[z]->connections[y]<<" ";
+                        // cout<<potentialLoopVerticies[z]->connections[y]<<" ";
                         if(potentialLoopVerticies[i]->connections[j] == potentialLoopVerticies[z]->connections[y]){
-                            cout<<"connection : "<<potentialLoopVerticies[i]->connections[j]<<" occur !!"<<endl;
+                            // cout<<"connection : "<<potentialLoopVerticies[i]->connections[j]<<" occur !!"<<endl;
                             repeatedConnections++;
                         }
-                    }cout<<endl;
+                    }
+                    // cout<<endl;
                 }
             }
 
-        }cout<<endl;       
+        }
+        // cout<<endl;       
     }
     
     if(repeatedConnections >= 6){
-        cout<<"there is a loop"<<endl;
+        // cout<<"there is a loop"<<endl;
+        return true;
     }else{
-        cout<<"there is no loop"<<endl;
+        // cout<<"there is no loop"<<endl;
+        return false;
     }
 
+    return false;
+
+
+}
+
+bool areAllVerticiesVisited(){
+    for(int i=0;i<graphSize;i++){
+            if(vv[i] == 0){
+                return false;
+            }
+    }
+    return true;
+}
+
+void clearVisitedVerticies(){
+    for(int i=0;i<graphSize;i++){
+        vv[i] = 0;
+    }
+}
+
+
+void kruskal(int **graph, int graphSize, int **newGraph){
+    while(areAllVerticiesVisited() == false){
+        // cout<<__func__<<endl;
+
+        clearVisitedVerticies();
+
+        vertexConnection* mv = findTheMinimumConnection(graph, graphSize);
+        
+        addTheConnectionsToTheNewGraph(newGraph, mv);
+        
+        if(isThereACycle(newGraph,graphSize) == false){
+            // cout<<"no cycle"<<endl;
+            removeTheConnectionsFromTheOldGraph(graph,mv);
+            
+            stack.push_back(mv->connectionFromVertex);
+            dfs(newGraph);
+            
+            // print(vv,graphSize);cout<<endl;
+
+        }else{
+            // cout<<"there will be a cycle remove the new connection"<<endl;
+            removeTheConnectionsFromTheOldGraph(newGraph,mv);
+        }
+        
+
+    }
+    
+    cout<<__func__<<" spanning tree"<<endl;
+    print(newGraph,graphSize);
+    cout<<endl;
+
+}
+
+
+int main(){
+    int graphCp[graphSize][graphSize] = {
+       //0 1 2 3
+        {0,1,2,6}, //0
+        {1,0,0,1}, //1
+        {2,0,0,3}, //2
+        {6,1,3,0}  //3
+	};    
     
     
     
 
+    int **graph = new int*[graphSize];
+    // make this function
+    for(int i=0; i<graphSize;i++){
+            graph[i] = new int[graphSize];
+    }
 
-    
-    
-    
+    //graph converted to int** graph
+    // make this function
+    for(int i=0;i<graphSize;i++){
+        for(int j=0;j<graphSize;j++){
+            graph[i][j] = graphCp[i][j];
+            // cout<<graph[i][j]<<" ";
+        }
+    }
+
+
+    // new graph to store the kruskal path
+    int **newGraph = new int*[graphSize];
+    for(int i=0;i<graphSize;i++){
+        newGraph[i] = new int[graphSize];
+    }
+
+    kruskal(graph, graphSize, newGraph);
 
 }
